45ec2496e9c12f19f63d6f1947386eab9dc438a4
Banh Quang Long <longlilom87@yahoo.com.vn>
2023-05-15 16:26:27 +0700

    highlighted navlink

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import {BrowserRouter,Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter,Routes, Route, NavLink} from 'react-router-dom'
 import Guide from './components/Guide'
 import Home from './components/Home'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  function activeClass({isActive}) {
+    if (isActive){
+      return 'active';
+    }
+  }
   return (
     <div className="App">
       <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand">COMPANY PORTAL</a>
+        <a className="navbar-brand" href='/'>COMPANY PORTAL</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
       {/* <a class="nav-item nav-link disabled" href="#">Disabled</a> */}
-      <Link className="nav-item nav-link active" to="/">Home</Link>
-      <Link className="nav-item nav-link " to="/guide">Guide</Link>  
+      <NavLink className={"nav-item nav-link "+activeClass} to="/">Home</NavLink>
+      <NavLink className={"nav-item nav-link "+activeClass} to="/guide">Guide</NavLink>  
       </div>
     </div>
 </nav>
